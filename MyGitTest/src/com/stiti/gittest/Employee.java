dd69fc6ad731aa7e20eab32a0eedfb55165c847b
stitip <stiti.pragnya@gmail.com>
2015-04-14 13:12:03 +0530

    Initial commit

---
+++ MyGitTest/src/com/stiti/gittest/Employee.java
@@ -0,0 +1,38 @@
+package com.stiti.gittest;
+
+public class Employee {
+
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String department;
+	
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+	
+}
---
+++ MyGitTest/src/com/stiti/gittest/Main.java
@@ -0,0 +1,17 @@
+package com.stiti.gittest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setEmpId(1);
+		emp.setFirstName("Stiti");
+		emp.setLastName("Samantray");
+		emp.setDepartment("Research and Development");
+		
+		System.out.println("Employee \"" + emp.getEmpId() + " - " + emp.getFirstName() + " " + emp.getLastName() + "\" added successfully.");
+
+	}
+
+}
